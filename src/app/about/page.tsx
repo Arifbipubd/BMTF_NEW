62f4e457230cfa66a5da2b5c4e66bc393e54cc97
Arifbipubd <arifulislambipubd@gmail.com>
2024-01-05 16:37:26 +0600

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import DemoComponent from "@src/components/Demo";
+
+export default function Home() {
+  return (
+    <div className=' text-red-500 '>
+      About Page <DemoComponent />
+    </div>
+  );
+}
---
+++ src/components/Demo.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className=' text-red-500 '>
+      <Link href='/'>Home</Link>
+    </div>
+  );
+}
