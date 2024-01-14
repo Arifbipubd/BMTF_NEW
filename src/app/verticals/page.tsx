0fb2bcb4baad71d63f3f4e9371296da0b3789321
Sajal13 <sajaldas138900@gmail.com>
2024-01-14 23:57:42 +0600

    netlify error

--- src/app/verticals/page.tsx
+++ src/app/verticals/page.tsx
@@ -8,7 +8,7 @@ import AllVerticals from '@src/components/verticals/AllVerticals'
 
 type Props = {}
 
-export default function page({}: Props) {
+export default function Page({}: Props) {
     const pathname = usePathname()
 
     const [pathName, setPathName] = useState<Array<string>>([]);
