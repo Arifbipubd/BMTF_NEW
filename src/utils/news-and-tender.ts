0016a66c5b60c768118719a09afb235b16aa61ea
Sajal13 <sajaldas138900@gmail.com>
2024-02-03 23:50:26 +0600

    News and corporate page design and vertical page modified

--- src/utils/news-and-tender.ts
+++ src/utils/news-and-tender.ts
@@ -45,7 +45,7 @@ export const newsAndTender: Array<any> = [
 
 export const singleNews: any = {
     1: {
-        imageSrc: "/assets/images/news-and-tender/news_1.png",
+        imageSrc: "/assets/images/news-and-tender/BMTF_News_1.jpg",
         title: "BMTF to provide 3 crore blank smart cards to EC",
         description: [
             `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
@@ -55,53 +55,80 @@ export const singleNews: any = {
         ],
     },
     2: {
-        imageSrc: "/assets/images/news-and-tender/news_2.png",
+        imageSrc: "/assets/images/news-and-tender/BMTF_News_2.jpg",
         title: "Chief of Army Staff inaugurates Army Pharma Limited in Gazipur",
         description: [
-            `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
-            `Cabinet Committee on Government Purchase in a meeting on Tuesday approved the proposal along with some others from different ministries.`,
-            `Finance minister AHM Mustafa Kamal presided over the virtual meeting.`,
-            `As per the proposal, the BMTF will supply the smart cards under the Identification System for Enhancing Access to Services project of the Arms Forces Division of the prime minister’s office at contract value of Tk 406.50 crore.`,
+            `Chief of Army Staff General Aziz Ahmed inaugurated Army Pharma Limited at Shimultali under Joydebpur in the Gazipur district on Thursday.`,
+            `Army Pharma Limited, a subsidiary of Bangladesh Machine Tools Factory Limited (BMTF), an organization run by the Bangladesh Army, started its official journey with the conviction of producing and marketing quality medicines, reports BSS citing an Inter-Services Public Relation (ISPR) press release issued on Friday.`,
+            `Chief of Army Staff General Aziz Ahmed termed as historic the journey of Army Pharma during the birth centenary of Father of the Nation Bangabandhu Sheikh Mujibur Rahman and the 50th anniversary of the country’s independence.`,
+            `Under the worthy leadership of Prime Minister Sheikh Hasina, Aziz hoped, Bangladesh Army and Army Pharma would play a significant role in the health sector in the days to come.`,
+            `He expressed his hope that the company will emerge as one of the top pharmaceutical companies in the country within the shortest possible time.`
         ],
     },
     3: {
-        imageSrc: "/assets/images/news-and-tender/news_3.png",
+        imageSrc: "/assets/images/news-and-tender/BMTF_News_3.jpg",
         title: "Bangladesh Machine Tools Factory Limited wins global ISO certification",
         description: [
-            `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
-            `Cabinet Committee on Government Purchase in a meeting on Tuesday approved the proposal along with some others from different ministries.`,
-            `Finance minister AHM Mustafa Kamal presided over the virtual meeting.`,
-            `As per the proposal, the BMTF will supply the smart cards under the Identification System for Enhancing Access to Services project of the Arms Forces Division of the prime minister’s office at contract value of Tk 406.50 crore.`,
+            `Bangladesh Machine Tools Factory Limited (BMTF), a state-owned limited company under the management of Bangladesh Army has been awarded the global ISO standard certification recently. BMTF is one of the largest commercial enterprises of its kind in Bangladesh located at Shimultoly of Gazipur Sadar, built as the mother industry of the country's small enterprises dealing with minor engineering products.`,
+            `Major General Sultanuzzaman Md. Saleh Uddin (Managing Director of BMTF), Brigadier General Md. Abdul Hye (Director Marketing), Colonel Md. Hasan Morshed (Addn Director- Planning & Training) and Major A K M Humayun Kabir (DGM-Planning & Training) while on behalf of UNICERT, Abdul Kader, Director of UNICERT, Engr. M Liaquat Ali, Director Asia Region of UNICERT's global operations and Lt. Colonel Maksudul Haq (rtd) along with other senior officials attended the certificate award ceremony at the BMTF corporate office, at the Dhaka Cantonment on February 15 last.`,
+            `UNICERT Director Abdul Kader said: "UNICERT is committed to bringing world class standards to Bangladesh and to offer management system certification services to the highest standard to all organizations without discrimination, by competent, consistent, impartial and value added conformity assessments and UNICERT is proud to certify the army owned company, BMTF, for two international certifications related to Quality and Health and Safety Management Systems." It may be mentioned here that to accelerate the industrialization of erstwhile East Pakistan, the factory started its journey in 1967 and after the independence, it was re-inaugurated on February 11, 1979 under the government management.`,
+            `Later Prime Minister Sheikh Hasina, officially handed over the factory to Bangladesh Army on July 27, 2000 to increase its efficiency. Since then BMTF is functioning as a State-Owned Limited Company. Recently it has been awarded with the with latest version of the internationally recognized ISO 9001:2015 (Quality Management System) and ISO 45001:2018 (Occupational Health & Safety Management System) certifications from the accredited certification body United Certification Services Limited - UNICERT.`,
+            `UNICERT is a private accredited certification body (CB) in Bangladesh accredited by the International Accreditation Services (IAS)-USA, the Department of Standards Malaysia (DSM), APMG-UK and The Bangladesh Accreditation Board (BAB). UNICERT is also the only CMMI transition partner in Bangladesh of the CMMI Institute, USA. It provides services to government and private organizations worldwide related to certification, inspection, testing and training on conformity assessments focused on key dynamics that shape organizational behavior.`,
+            `UNICERT awarded the international ISO certifications to BMTF in recognition of its outstanding achievements and commitment to international standards in factory operation compliance as assessed by UNICERT.`
         ],
     },
     4: {
-        imageSrc: "/assets/images/news-and-tender/news_4.png",
+        imageSrc: "/assets/images/news-and-tender/DNCC-LED.jpg",
         title: "DNCC LED Lights Supply & Installation",
         description: [
-            `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
-            `Cabinet Committee on Government Purchase in a meeting on Tuesday approved the proposal along with some others from different ministries.`,
-            `Finance minister AHM Mustafa Kamal presided over the virtual meeting.`,
-            `As per the proposal, the BMTF will supply the smart cards under the Identification System for Enhancing Access to Services project of the Arms Forces Division of the prime minister’s office at contract value of Tk 406.50 crore.`,
+            `The Dhaka North City Corporation (DNCC) will complete installing 46,410 LED lights in the city streets by 2021 under the 'LED Light Supply and Installation in DNCC Streets' project.`,
+            `The revised cost of the project is over Tk 3.69 billion. The project was supposed to complete by December 31 this year which may be extended due to the COVID-19 pandemic.`,
+            `The DNCC signed an agreement with Bangladesh Machine Tools Factory (BMTF) in this regard on Tuesday at a program at Nagar Bhaban in the city's Gulshan.`,
+            `Project director and DNCC superintendent engineer Rafiqul Islam and BMTF general manager Lt Col Tofael Ahmed signed the deal on behalf of their respective organizations.`,
+            `DNCC mayor Atiqul Islam was present during signing of the agreement.`,
+            `Mr Atiqul Islam said that 42,500 LED lights can be installed by 2021. Some of the streets will be illuminated by January 01, he said.`,
+            `"It is part of our pledge of making the city safe by illuminating the city. The lights are made in Europe with 10 years of warranty," he added.`,
+            `The BMTF will complete the work efficiently and do the maintenance during warranty period.`,
+            `The LED light installation project (1st amended) was approved in September last year at a cost of over Tk 3.69 billion. The number of LED lights was mentioned 42,450 in the original project which was later increased to 46,410.`,
+            `Of those, 3,408 will be of 150 watts, 3,646 of 90 watt, 10,666 of 60 watt and 25,661 of 40 watt.`,
+            `The cost of lights installation has been fixed at more than Tk 3.19 billion. This includes purchase of lights, pole purchase and installation, bracket, fittings, controlling system, software and overhead cables.`,
+            `The lights have been imported from Poland. The rest Tk 494 million will be spent for purchase of hydraulic car and other vehicles, machinery and equipment.`
         ],
     },
     5: {
-        imageSrc: "/assets/images/news-and-tender/news_5.png",
+        imageSrc: "/assets/images/news-and-tender/Jhilmil-Residential-Park.jpg",
         title: "Jhilmil Residential Park Project",
         description: [
-            `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
-            `Cabinet Committee on Government Purchase in a meeting on Tuesday approved the proposal along with some others from different ministries.`,
-            `Finance minister AHM Mustafa Kamal presided over the virtual meeting.`,
-            `As per the proposal, the BMTF will supply the smart cards under the Identification System for Enhancing Access to Services project of the Arms Forces Division of the prime minister’s office at contract value of Tk 406.50 crore.`,
+            `SVC Jhilmil Residential BD Limited and Bangladesh Machine Tools Factory Limited (BMTF) have collaborated for the 'Jhilmil Residential Park Project Technology Transfer'.`,
+            `An announcement ceremony was held at Pan Pacific Sonargaon in the capital on Wednesday, said a press release issued on Wednesday.`,
+            `The Chairman of SVC Jhilmil Residential BD Limited, Professor Dr Sharifah Sabrina, and Brigadier General Md Arman Hossain , SGP,BGBM,Ndc,Psc , Director of BMTF, signed the Collaboration as their organization's representative.`,
+            `Dr Sharifah Sabrina welcomed everyone with a warm address, followed by an overview of the collaboration by BMTF.`,
+            `Private Partner from Malaysia working with Rajuk, the Contracting Authority for the PPP Project Jhilmil Residential Park, is poised to leap frog into implementing the Project with Technology Transfer from Malaysia to Bangladesh in most of its procurements for the Project.`,
+            `Having understood the current economic conditions and the need for foreign exchange in Bangladesh, BNG Global Holding has resorted to manufacture some of the advanced and innovative construction material in Bangladesh, thus avoiding the economic drain as a result of heavy taxes and also containing the foreign currency in the country.`,
+            `Technology transfer is one of the key components of commitment and collaboration in preparing the Development Index around diverse issues, there is no natural tendency to make global economic growth a win-win enterprise, and the consumption of knowledge is no longer non-rival. Finding smarter options to allay these fears and to accelerate efforts to ensure that everyone, everywhere, can take the advantage of technological progress and thereby reduce inequality was the thrust the Private Partner took upon them, reads the release.`,
+            `They made smart technology transfer of manufacturing spun Piles in the premises of BMITF, a concern of the Bangladesh Army.`,
+            `The Ministry of Housing and Public Works welcomed this progress towards the quick implementation of the Project.`,
+            `Much has been spoken of the delay to get the project off the ground. The two years of pandemic delayed the whole implementation process and the shipping of all procurements from Malaysia as was the intention of BNG Global Holding was also hampered with the global economic crisis coupled with heavy taxation making it so difficult to leap frog into the project implementation, with their desired new construction technology, away from the mundane brick and mortar that is commonly used in building residential projects in Bangladesh`,
+            `The determination of the Private Partner BNG to give the people of Bangladesh a better home has been consistent. Hence, they are endeavoring to leap frog into the production of spun piles and also the Industrialized Building system as is known in Malaysia. This technique of construction avoids all the mishaps of a traditional construction method that would have to be dismantled unlike in IBS. It also reduces manual labor, shortens to great extent construction tenure, and offers a clean environment to work in. BNG is planning on driving the piles three months from now when the first batch of piles will be manufactured and brought to site for driving. The piling work offers great speed in completion without worrying about weather conditions, and logistics. Homes built with this technology offer homeowners strong concrete walls resistant to earthquakes and have stood the test of time.`,
+            `The project to handover to Rajuk 13700 apartments in 4 years would have been achieved as the Private Partner BNG Global Holding had all of the resources to spearhead the Project but the unavoidable natural calamity of pandemic and war was totally unanticipated.`,
+            `Today the Company is poised to make its Dream come true and achieve its sustainable development goal motto of Connecting Communities.`
         ],
     },
     6: {
-        imageSrc: "/assets/images/news-and-tender/news_6.png",
+        imageSrc: "/assets/images/news-and-tender/Solar-Home-System.jpg",
         title: "Solar Home System Supply & Installation",
         description: [
-            `Bangladesh Army-run Bangladesh Machine Tools Factory Limited will supply 3 crore blank smart cards to Bangladesh Election Commission.`,
-            `Cabinet Committee on Government Purchase in a meeting on Tuesday approved the proposal along with some others from different ministries.`,
+            `The Cabinet Committee on Public Purchase on Wednesday approved four proposals including one for installing 42,500 solar systems in the country’s three hill districts.`,
+            `Of these, 40,000 are solar home systems (SHS), each having 100 watt-peak capacity, while the remaining 2,500 are solar community systems, each having 320 watt-peak capacity.`,
             `Finance minister AHM Mustafa Kamal presided over the virtual meeting.`,
-            `As per the proposal, the BMTF will supply the smart cards under the Identification System for Enhancing Access to Services project of the Arms Forces Division of the prime minister’s office at contract value of Tk 406.50 crore.`,
+            `According to officials, the Parbatya Chattagram Unnayan Board (PCUB) under the Ministry of Chittagong Hill Tracts Affairs (MCHTA) will install solar home and community systems at a cost of Tk204.37 crore.`,
+            `Bangladesh Machine Tools Factory (BMTF) Ltd, an enterprise of Bangladesh Army, won the contract through direct procurement method (DPM) to supply and install the solar systems.`,
+            `Under the method, any government entity can purchase any product or service through negotiation without any bidding process.`,
+            `A senior official at MCHTA said that the same proposal was approved by the Cabinet body on April 29 to award the contract to the Border Guard of Bangladesh (BGB) Welfare Trust in the same process.`,
+            `He added: “But after objections from the Ministry of Home Affairs, the contract was cancelled and later awarded to BMTF through negotiation.”`,
+            `Kazi Mokhlesur Rahman, deputy secretary of the MCHTA, said: “About 40,000 families at the off-grid remote areas will benefit from the solar home systems.”`,
+            `He said: “The government will provide the solar systems to the families free of cost which will enable a consumer to use four LED bulbs, each having 3 watt capacity, a mobile recharging panel, a 12 volt fan and a small TV.”`,
+            `There will be a battery to provide 5-6 hours backup power supply for the solar system.`
         ],
     },
 };
