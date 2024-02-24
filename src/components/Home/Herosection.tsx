76961ab5ae50bd13f85a1cbcfeeee0eb5df2d78c
Sajal13 <sajaldas138900@gmail.com>
2024-02-25 00:53:03 +0600

    hero section animation

--- src/components/Home/Herosection.tsx
+++ src/components/Home/Herosection.tsx
@@ -6,9 +6,7 @@ import { heroSection } from "@src/utils/constants";
 
 type Props = {};
 
-const variant = {
-
-}
+const variant = {};
 
 export default function Herosection({}: Props) {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -30,69 +28,128 @@ export default function Herosection({}: Props) {
     }, [currentSlide]);
     return (
         <Fragment>
-            <div className="relative z-0">
+            <div className="relative z-0 overflow-hidden bg-black/90">
                 <div className="container mx-auto grid grid-cols-2 gap-4">
                     <div className="min-h-[90vh] md:min-h-screen overflow-hidden">
                         {heroSection.map((slide, index) => (
                             <Fragment key={slide.id}>
                                 <AnimatePresence>
-                                    <motion.div
-                                        className={`absolute top-0 left-0 w-full  ${
-                                            index === currentSlide
-                                                ? "block"
-                                                : "hidden"
-                                        } transition-opacity duration-500 ${
-                                            index === currentSlide ? "" : ""
-                                        } h-full`}
-                                        
-                                    >
-                                        {slide.assetUrl.includes(".mp4") ? (
-                                            <video
-                                                autoPlay
-                                                loop
-                                                muted
-                                                className="absolute inset-0 object-cover w-screen h-full -z-20"
-                                            >
-                                                <source
-                                                    src={slide.assetUrl}
-                                                    type="video/mp4"
-                                                />
-                                            </video>
-                                        ) : (
-                                            <div className="absolute inset-0 w-full h-full -z-20">
-                                                {/*eslint-disable-next-line @next/next/no-img-element*/}
-                                                <img
-                                                    src={slide.assetUrl}
-                                                    alt=""
-                                                    className="h-full w-full object-cover"
-                                                />
-                                            </div>
-                                        )}
-                                        <div className="bg-[rgba(16,25,48,0.70)] absolute left-0 w-full h-full -z-10" />
-                                        <div className="flex justify-center h-full">
-                                            <div className="container mx-auto flex flex-col justify-center h-full z-20">
-                                                <div className="w-[80%] xl:w-[60%]">
-                                                    <div className="">
-                                                        <div className="bg-[rgba(211,170,20,0.20)] px-1.5 lg:px-2.5 py-3 lg:py-[15px] w-fit rounded-[100px] mb-3 lg:mb-4">
-                                                            <p className="text-secondary text-sm font-semibold">
-                                                                BMTF Stories
-                                                            </p>
-                                                        </div>
-                                                        <h1
-                                                            className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[52px]
+                                    {currentSlide === index && (
+                                        <motion.div
+                                            className={`absolute top-0 left-0 w-full h-full`}
+                                            initial={{ x: -100, opacity: 0 }}
+                                            animate={{ x: 0, opacity: 1 }}
+                                            exit={{ x: 100, opacity: 0 }}
+                                            transition={{
+                                                ease: "easeInOut",
+                                                duration: 1
+                                            }}
+                                        >
+                                            {slide.assetUrl.includes(".mp4") ? (
+                                                <video
+                                                    autoPlay
+                                                    loop
+                                                    muted
+                                                    className="absolute inset-0 object-cover w-screen h-full -z-20"
+                                                >
+                                                    <source
+                                                        src={slide.assetUrl}
+                                                        type="video/mp4"
+                                                    />
+                                                </video>
+                                            ) : (
+                                                <div className="absolute inset-0 w-full h-full -z-20">
+                                                    {/*eslint-disable-next-line @next/next/no-img-element*/}
+                                                    <img
+                                                        src={slide.assetUrl}
+                                                        alt=""
+                                                        className="h-full w-full object-cover"
+                                                    />
+                                                </div>
+                                            )}
+                                            <div className="bg-[rgba(16,25,48,0.70)] absolute left-0 w-full h-full -z-10" />
+                                            <div className="flex justify-center h-full">
+                                                <div className="container mx-auto flex flex-col justify-center h-full z-20">
+                                                    <div className="w-[80%] xl:w-[60%]">
+                                                        <div className="">
+                                                            <motion.div
+                                                                className="bg-[rgba(211,170,20,0.20)] px-1.5 lg:px-2.5 py-3 lg:py-[15px] w-fit rounded-[100px] mb-3 lg:mb-4"
+                                                                initial={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                animate={{
+                                                                    scale: 1,
+                                                                    opacity: 1,
+                                                                }}
+                                                                exit={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                transition={{
+                                                                    ease: "easeInOut",
+                                                                    duration: 0.5,
+                                                                    delay: 1,
+                                                                }}
+                                                            >
+                                                                <p className="text-secondary text-sm font-semibold">
+                                                                    BMTF Stories
+                                                                </p>
+                                                            </motion.div>
+                                                            <motion.h1
+                                                                className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[52px]
                                          text-white font-extrabold lg:leading-[56px] capitalize 
                                          mb-6 lg:mb-[30px]`}
-                                                        >
-                                                            {slide.heading}
-                                                        </h1>
-                                                        <p className="text-white text-sm md:text-base 2xl:text-lg text-justify lg:leading-[24px]">
-                                                            {slide.description}
-                                                        </p>
+                                                                initial={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                animate={{
+                                                                    scale: 1,
+                                                                    opacity: 1,
+                                                                }}
+                                                                exit={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                transition={{
+                                                                    ease: "easeInOut",
+                                                                    duration: 0.5,
+                                                                    delay: 1,
+                                                                }}
+                                                            >
+                                                                {slide.heading}
+                                                            </motion.h1>
+                                                            <motion.p
+                                                                className="text-white text-sm md:text-base 2xl:text-lg text-justify lg:leading-[24px]"
+                                                                initial={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                animate={{
+                                                                    scale: 1,
+                                                                    opacity: 1,
+                                                                }}
+                                                                exit={{
+                                                                    scale: 0,
+                                                                    opacity: 0,
+                                                                }}
+                                                                transition={{
+                                                                    ease: "easeInOut",
+                                                                    duration: 0.5,
+                                                                    delay: 1.5,
+                                                                }}
+                                                            >
+                                                                {
+                                                                    slide.description
+                                                                }
+                                                            </motion.p>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </motion.div>
+                                        </motion.div>
+                                    )}
                                 </AnimatePresence>
                             </Fragment>
                         ))}
